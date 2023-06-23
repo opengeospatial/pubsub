db099238120c0d1d0ffe728e2bcf41c686c280a2
Gobe Hobona <ghobona@opengeospatial.org>
2023-06-23 19:42:28 +0100

    change batch script to build with asciidoctor

--- charter/build.sh
+++ charter/build.sh
@@ -1,1 +1,2 @@
-docker run -v "$(pwd)":/metanorma metanorma/mn metanorma compile --agree-to-terms -t ogc -x xml,html,doc,pdf PubSub_SWG_Charter.adoc
+docker run --rm -v "$(pwd)":/documents/ asciidoctor/docker-asciidoctor asciidoctor PubSub_SWG_Charter.adoc
+docker run --rm -v "$(pwd)":/documents/ asciidoctor/docker-asciidoctor asciidoctor PubSub_SWG_Charter.adoc
