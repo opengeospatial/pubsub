9f8e3ad32a0652b0920ac21a418a1a220fe3b427
Tom Kralidis <tomkralidis@gmail.com>
2025-01-08 09:31:15 -0500

    remove best practice, move SWG Charter to root directory (#12)
    * remove best practice
    
    * move SWG charter to root directory
    
    * Revert "move SWG charter to root directory"
    
    This reverts commit f5a155c3ea2d95fc6d36d499a64bb695c9c011f0.
    
    * move charter to root directory

--- charter/build.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -v "$(pwd)":/documents/ asciidoctor/docker-asciidoctor asciidoctor PubSub_SWG_Charter.adoc
-docker run --rm -v "$(pwd)":/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf PubSub_SWG_Charter.adoc
